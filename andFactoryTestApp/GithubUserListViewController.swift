ae61b7289396624126a76a23e1333a3b4b073c12
tanaka-yuki-dc <tanaka.y@doublecrown.jp>
2022-08-22 22:40:05 +0900

    tableViewに紐付け、その他コメント追加

--- andFactoryTestApp/GithubUserListViewController.swift
+++ andFactoryTestApp/GithubUserListViewController.swift
@@ -16,13 +16,38 @@ class GithubUserListViewController: UIViewController {
     super.viewDidLoad()
     // ナビゲーションバーの設定
     naviBarSetUp()
+    // スクロールで検索キーボード閉じる
     tableView.keyboardDismissMode = .onDrag
+    tableView.delegate = self
+    tableView.dataSource = self
+    tableView.rowHeight = UITableView.automaticDimension
     searchBar.delegate = self
+    
+    tableView.register(UINib(nibName: GithubUserListTableViewCell.identifier, bundle: nil), forCellReuseIdentifier: GithubUserListTableViewCell.identifier)
+    
   }
 }
 
-extension GithubUserListViewController: UISearchBarDelegate {
+//MARK: - tableView処理
+extension GithubUserListViewController: UITableViewDelegate, UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 1
+  }
   
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return self.tableView.bounds.height / 10
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+     let cell = tableView.dequeueReusableCell(withIdentifier: GithubUserListTableViewCell.identifier, for: indexPath) as! GithubUserListTableViewCell
+    cell.setUp()
+    return cell
+  }
+  
+}
+
+//MARK: - 検索バー処理
+extension GithubUserListViewController: UISearchBarDelegate {
   /// 検索ボタン押下
   func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
     // キーボードを閉じる
@@ -30,6 +55,7 @@ extension GithubUserListViewController: UISearchBarDelegate {
   }
 }
 
+// MARK: - 画面初期設定
 extension GithubUserListViewController {
   /// ナビゲーションバーの設定
   private func naviBarSetUp() {
