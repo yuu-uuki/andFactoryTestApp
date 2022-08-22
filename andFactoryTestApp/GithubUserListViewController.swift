5fb4124bd364fae9a3edee418ae424d7f60fbe02
tanaka-yuki-dc <tanaka.y@doublecrown.jp>
2022-08-22 20:54:38 +0900

    アプリの作成

---
+++ andFactoryTestApp/GithubUserListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  andFactoryTestApp
+//
+//  Created by 田中裕貴 on 2022/08/22.
+//
+
+import UIKit
+
+class GithubUserListViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ andFactoryTestApp/Multilingual.swift
@@ -0,0 +1,8 @@
+//
+//  Multilingual.swift
+//  andFactoryTestApp
+//
+//  Created by 田中裕貴 on 2022/08/22.
+//
+
+import Foundation
