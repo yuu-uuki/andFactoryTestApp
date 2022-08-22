6413d5ce7a7c03e8c8471609404e5c21b5674fa9
tanaka-yuki-dc <tanaka.y@doublecrown.jp>
2022-08-22 20:55:24 +0900

    UIを配置

--- andFactoryTestApp/GithubUserListViewController.swift
+++ andFactoryTestApp/GithubUserListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GithubUserListViewController.swift
 //  andFactoryTestApp
 //
 //  Created by 田中裕貴 on 2022/08/22.
@@ -8,12 +8,35 @@
 import UIKit
 
 class GithubUserListViewController: UIViewController {
-
+  
+  @IBOutlet weak var tableView: UITableView!
+  @IBOutlet weak var searchBar: UISearchBar!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    // ナビゲーションバーの設定
+    naviBarSetUp()
+    tableView.keyboardDismissMode = .onDrag
+    searchBar.delegate = self
   }
+}
 
+extension GithubUserListViewController: UISearchBarDelegate {
+  
+  /// 検索ボタン押下
+  func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+    // キーボードを閉じる
+    view.endEditing(true)
+  }
+}
 
+extension GithubUserListViewController {
+  /// ナビゲーションバーの設定
+  private func naviBarSetUp() {
+    let appearance = UINavigationBarAppearance()
+    appearance.backgroundColor = .white
+    UINavigationBar.appearance().scrollEdgeAppearance = appearance
+    navigationItem.title = Titlename.listView
+  }
 }
 
--- andFactoryTestApp/Multilingual.swift
+++ andFactoryTestApp/Multilingual.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+
+struct Titlename {
+  static let listView = "search user"
+}
