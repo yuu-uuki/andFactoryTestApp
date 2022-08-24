2bec1ae138448f39d24d6fac73c6d0de1aceabee
tanaka-yuki-dc <tanaka.y@doublecrown.jp>
2022-08-24 23:20:53 +0900

    webView画面追加

---
+++ andFactoryTestApp/GithubUserDetailViewController/GithubUserDetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  GithubUserDetailViewController.swift
+//  andFactoryTestApp
+//
+//  Created by 田中裕貴 on 2022/08/24.
+//
+
+import UIKit
+
+class GithubUserDetailViewController: UIViewController {
+  
+  public static let identifer = "GithubUserDetailViewController"
+  public var url: URL?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      print(url)
+    }
+    
+
+}
