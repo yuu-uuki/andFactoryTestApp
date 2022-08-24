ed04a190fe60872b0561266ffc6c7ec6b8258b70
tanaka-yuki-dc <tanaka.y@doublecrown.jp>
2022-08-25 00:49:46 +0900

    webView表示処理、エラーハンドリング追加

--- andFactoryTestApp/GithubUserDetailViewController/GithubUserDetailViewController.swift
+++ andFactoryTestApp/GithubUserDetailViewController/GithubUserDetailViewController.swift
@@ -6,16 +6,71 @@
 //
 
 import UIKit
+import WebKit
 
 class GithubUserDetailViewController: UIViewController {
   
-  public static let identifer = "GithubUserDetailViewController"
-  public var url: URL?
+  @IBOutlet weak var webView: WKWebView! {
+    didSet {
+      webView.navigationDelegate = self
+    }
+  }
+  private var indicatorView = UIActivityIndicatorView()
+  static let identifer = "GithubUserDetailViewController"
+  public var htmlUrl: URL?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-      print(url)
+      setUpUI()
+      if let url = self.htmlUrl {
+        indicatorView.startAnimating()
+        
+        var request = URLRequest(url: url)
+        request.cachePolicy = .useProtocolCachePolicy
+        request.timeoutInterval = 1
+        self.webView.load(request)
+      }
     }
-    
+}
 
+// MARK: - webView
+extension GithubUserDetailViewController: WKNavigationDelegate {
+  
+  func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+    guard let url = navigationAction.request.url else {
+      return
+    }
+    print("リクエストURL: \(url)")
+    decisionHandler(.allow)
+  }
+  
+  // 読み込み終了
+  func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+    print("読み込み終了")
+    indicatorView.stopAnimating()
+  }
+  
+  func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+    print("読み込み失敗(読み込み開始時)\(error)")
+    // アラートを表示
+    UIAlertHelper(title: Error.AlertTitle.error, message: Error.Alertmessage.loadingError).makeSingleAlert(self, okClosure: nil).show()
+    indicatorView.stopAnimating()
+  }
+  
+  func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+    print("読み込み失敗(読み込み途中)\(error)")
+    // アラートを表示
+    UIAlertHelper(title: Error.AlertTitle.error, message: Error.Alertmessage.loadingError).makeSingleAlert(self, okClosure: nil).show()
+    indicatorView.stopAnimating()
+  }
+}
+
+extension GithubUserDetailViewController {
+  func setUpUI() {
+    // インジケータ設定
+    indicatorView.center = view.center
+    indicatorView.style = .large
+    indicatorView.color = .black
+    view.addSubview(indicatorView)
+  }
 }
