af579f73b0a50bbc43376d8b371d4f26aac6bc90
tanaka-yuki-dc <tanaka.y@doublecrown.jp>
2022-08-24 22:31:32 +0900

    Model作成

---
+++ andFactoryTestApp/Users.swift
@@ -0,0 +1,28 @@
+//
+//  Users.swift
+//  andFactoryTestApp
+//
+//  Created by 田中裕貴 on 2022/08/22.
+//
+
+import Foundation
+
+struct Users: Codable {
+  let items: [User]
+}
+
+struct User: Codable {
+  
+  let login: String
+  let avatarURL: URL
+  let htmlUrl: String
+  let type: String
+  
+  private enum CodingKeys: String, CodingKey {
+    case login
+    case avatarURL = "avatar_url"
+    case htmlUrl = "html_url"
+    case type
+  }
+  
+}
